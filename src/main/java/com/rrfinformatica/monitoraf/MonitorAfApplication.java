654db320455b30c9dd252ea928f131b59ea2bc2b
Diego Reyes <diegoreyesmo@gmail.com>
2022-04-21 16:13:38 -0400

    carga inicial proyecto

---
+++ src/main/java/com/rrfinformatica/monitoraf/MonitorAfApplication.java
@@ -0,0 +1,13 @@
+package com.rrfinformatica.monitoraf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorAfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorAfApplication.class, args);
+	}
+
+}

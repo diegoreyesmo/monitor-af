6c0ea4ed1f6ee46c1ba550d0444d4102cb3b65f5
Diego Reyes <diegoreyesmo@gmail.com>
2022-04-29 11:20:55 -0400

    fix findAll

--- src/main/java/com/rrfinformatica/monitoraf/RegistroRepository.java
+++ src/main/java/com/rrfinformatica/monitoraf/RegistroRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 
 public interface RegistroRepository extends CrudRepository<Registro, Long> {
-    List<Registro> findAll(String creadoPor);
+    List<Registro> findAll();
     List<Registro> findByUsuario(String usuario);
     ActividadFisica findById(long id);
 }

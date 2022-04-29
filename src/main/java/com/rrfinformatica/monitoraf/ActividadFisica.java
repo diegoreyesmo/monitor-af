4ca11d0ea7fa2ebba60fcf1883efddb075891c62
Diego Reyes <diegoreyesmo@gmail.com>
2022-04-29 11:26:04 -0400

    add getter/setter

--- src/main/java/com/rrfinformatica/monitoraf/ActividadFisica.java
+++ src/main/java/com/rrfinformatica/monitoraf/ActividadFisica.java
@@ -17,4 +17,35 @@ public class ActividadFisica {
     @NotNull
     private String creadoPor;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getActividad() {
+        return actividad;
+    }
+
+    public void setActividad(String actividad) {
+        this.actividad = actividad;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getCreadoPor() {
+        return creadoPor;
+    }
+
+    public void setCreadoPor(String creadoPor) {
+        this.creadoPor = creadoPor;
+    }
 }
--- src/main/java/com/rrfinformatica/monitoraf/Registro.java
+++ src/main/java/com/rrfinformatica/monitoraf/Registro.java
@@ -20,4 +20,43 @@ public class Registro {
     @NotNull
     private Integer duracion;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getActividad() {
+        return actividad;
+    }
+
+    public void setActividad(String actividad) {
+        this.actividad = actividad;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public Integer getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(Integer duracion) {
+        this.duracion = duracion;
+    }
 }
